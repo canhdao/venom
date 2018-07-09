fed0e403eaa1545bac90765ce97101186b64df3e
Canh <daongoccanh@gmail.com>
2018-07-10 00:21:22 +0700

    + idle, ultimate

---
+++ Assets/SCR_Gameplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Gameplay : MonoBehaviour {
+	public GameObject venom;
+
+	// Use this for initialization
+	void Start () {
+		/*
+		const float REF_WIDTH = 1080;
+		// const float REF_HEIGHT = 1920;
+
+		// Fixed width
+		float h = (float)Screen.height / Screen.width * REF_WIDTH;
+		Camera.main.orthographicSize = 0.005f * h;
+		*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			venom.GetComponent<Animator>().SetTrigger("ultimate");
+		}
+	}
+}
