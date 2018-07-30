2b7e4729b48ac4848462a2f2e2c596c6a80d794a
canhdao <daongoccanh@gmail.com>
2018-07-30 10:47:59 +0700

    * wrong commit

--- Assets/SCR_Config.cs
+++ Assets/SCR_Config.cs
@@ -8,9 +8,9 @@ public class SCR_Config {
 	public readonly static float[] RATE_SOLDIER_1	= new float[]	{25, 20, 15, 15, 15, 15};
 	public readonly static float[] RATE_SOLDIER_2	= new float[]	{25, 20, 15, 15, 15, 15};
 	public readonly static float[] RATE_SOLDIER_3	= new float[]	{25, 20, 15, 15, 15, 15};
-	public readonly static float[] RATE_SOLDIER_4	= new float[]	{0, 20, 15, 15, 15, 15};
+	public readonly static float[] RATE_SOLDIER_4	= new float[]	{25, 20, 15, 15, 15, 15};
 	public readonly static float[] RATE_DOG_SOLDIER	= new float[]	{ 0,  0, 20, 10, 10, 10};
-	public readonly static float[] RATE_TRUCK		= new float[]	{ 25,  0,  0, 10, 10, 10};
+	public readonly static float[] RATE_TRUCK		= new float[]	{ 0,  0,  0, 10, 10, 10};
 	public readonly static float[] RATE_GIRL		= new float[]	{ 0, 20, 20, 20, 20, 20};
 
 	public const float RATE_PATTERN_2 = 20;
